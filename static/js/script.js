32d0e086ecac4ba7c8ae8da3841dc780b05592e1
Sai Sivaraju IN <iassivaraju.ss@gmail.com>
2025-12-12 17:52:30 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -23,4 +23,30 @@ document.addEventListener('DOMContentLoaded', function () {
             this.nextElementSibling.style.display = this.parentElement.classList.contains('open') ? 'block' : 'none';
         });
     });
+
+    // Cookie handling utilities
+    window.setCookie = function(name, value, days) {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    };
+
+    window.getCookie = function(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    };
+
+    window.eraseCookie = function(name) {
+        document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    };
 });
